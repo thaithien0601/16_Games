24bdadc41daa28947480c5bd9ea4eade73d8bd23
Thái Thiện <trandinhthaithien@dtu.edu.vn>
2022-01-24 12:30:28 +0700

    first commit

---
+++ 01 Tetris/tong.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+using namespace std;
+int main()
+{
+int a = 3;
+int b = 4;
+cout<<"a + b = "<< a+b ;
+return 0;
+}
---
+++ 04 Snake/Untitled1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+public:
+    Student(string n, int a) {
+        name = n;
+        age = a;
+    }
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s1("Long", 24);
+    Student s2("Kien", 29);
+    s1.display();
+    s2.display();
+    return 0;
+}
